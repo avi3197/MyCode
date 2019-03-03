c07fc55fcc8d9c518e3e744443d0e23d4362edfd
Avinash Mahajan <avinashmahajan9417@gmail.com>
2019-03-03 18:30:02 +0530

    commit

--- WelcomeToJava.java
+++ WelcomeToJava.java
@@ -4,7 +4,7 @@ public class WelcomeToJava
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello, World.");
+		System.out.println("Hello, CJC.");
 		System.out.println("Hello, Java.");
 	}
 }
